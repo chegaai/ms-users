0a53b534a901558da11ba2c810745be2021cb108
Lucas Santos <lhs.santoss@gmail.com>
2019-09-25 21:23:15 -0300

    Change create route name

--- src/presentation/app.ts
+++ src/presentation/app.ts
@@ -16,7 +16,7 @@ export const app = expresso(async (app, config: IAppConfig, environment: string)
 
   app.get('/users/:userId', routes.find(services.user))
   app.get('/users', routes.listAll(services.user))
-  app.post('/users', routes.create(services.user))
+  app.post('/signup', routes.create(services.user))
   app.post('/login', routes.login(services.user))
   app.put('/users/:userId', routes.update(services.user))
   app.delete('/users/:userId', routes.remove(services.user))
